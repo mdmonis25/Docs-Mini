72a40c8b43931e87bfe5b20459a3e2c63dbe4c5f
Md <mdmonis128@gmail.com>
2024-02-13 19:40:48 +0530

    Card Completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,15 +4,20 @@ import { LuDownload } from "react-icons/lu";
 export const Card = () => {
   return (
     <>
-      <div className="relative w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden">
+      <div className="relative w-60 h-72 rounded-[40px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden">
         <FaRegFileAlt />
         <p className="text-sm leading-tight mt-5 font-semibold">
           Lorem ipsum dolor sit amet consectetur adipisicing.
         </p>
-        <div className="footer absolute bottom-0 w-full py-3 px-8 left-0">
-          <div className="flex items-center justify-between mb-5">
+        <div className="footer absolute bottom-0 w-full left-0">
+          <div className="flex items-center justify-between mb-3 py-3 px-8 ">
             <h5>.4mb</h5>
-            <LuDownload />
+            <span className="w-6 h-6 bg-zinc-600 rounded-full flex justify-center items-center">
+              <LuDownload size=".7em" color="#fff" />
+            </span>
+          </div>
+          <div className="tag w-full py-4 bg-green-600 flex items-center justify-center">
+            <h3 className="text-sm font-semibold">Download Now</h3>
           </div>
         </div>
       </div>
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card } from './Card'
 
 const Foreground = () => {
+  
   return (
     <>
     <div className="fixed top-0 left-0 z-[3] w-full h-full"></div>
